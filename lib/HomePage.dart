c2f7e94f40f532bdb09be6b43e35522e511b8e39
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-03-24 14:35:17 -0400

    add Flip Card

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,16 +12,72 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  int time = 0;
+  Timer? timer;
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Image(image: AssetImage('assets/imgs/0.jpg')),
-            Text('da'),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image(image: AssetImage('assets/imgs/0.jpg')),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Tiempo $time',
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(12.0),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: 12,
+                  itemBuilder: (BuildContext context, int index) => FlipCard(
+                    onFlip: () {},
+                    direction: FlipDirection.HORIZONTAL,
+                    flipOnTouch: true,
+                    front: Container(
+                      margin: EdgeInsets.all(4.0),
+                      color: Colors.deepOrange.withOpacity(0.3),
+                    ),
+                    back: Container(
+                      margin: EdgeInsets.all(4.0),
+                      color: Colors.deepOrange,
+                      child: Center(
+                        child: Text(
+                          '$index',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  contador() {
+    timer = Timer.periodic(Duration(seconds: 1), (t) {
+      setState(() {
+        time = time + 1;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    contador();
   }
 }
